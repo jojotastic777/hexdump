155e90bec4e120fbface7ed2c2baeb5e67d4e1e5
jojotastic777 <jojotastic777@gmail.com>
2025-06-29 22:01:02 -0700

    Move /hexdump command onto the client. Resolves #1

--- common/src/main/kotlin/io/github/jojotastic777/hexdump/Hexdump.kt
+++ common/src/main/kotlin/io/github/jojotastic777/hexdump/Hexdump.kt
@@ -2,16 +2,9 @@ package io.github.jojotastic777.hexdump
 
 import io.github.jojotastic777.hexdump.config.HexdumpConfig
 import io.github.jojotastic777.hexdump.networking.HexdumpNetworking
-import at.petrak.hexcasting.api.mod.HexTags
-import at.petrak.hexcasting.api.utils.isOfTag
-import at.petrak.hexcasting.xplat.IXplatAbstractions
-import com.google.gson.Gson
-import dev.architectury.event.events.common.CommandRegistrationEvent
 import net.minecraft.resources.ResourceLocation
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
-import net.minecraft.commands.Commands.literal
-import net.minecraft.network.chat.Component
 
 object Hexdump {
     const val MODID = "hexdump"
@@ -25,36 +18,5 @@ object Hexdump {
     fun init() {
         HexdumpConfig.init()
         HexdumpNetworking.init()
-
-        val actionsRegistry = IXplatAbstractions.INSTANCE.actionRegistry
-        val gson = Gson()
-
-        CommandRegistrationEvent.EVENT.register { dispatcher, _, _ ->
-            dispatcher.register(literal("hexdump")
-                .executes { context ->
-                    val patterns = HashMap<String, Any>()
-
-                    for (id in actionsRegistry.registryKeySet()) {
-                        val entry = actionsRegistry.get(id)
-                        if (entry == null) { continue }
-
-                        val obj = object {
-                            val id = id.location().toString()
-                            val startDir = entry.prototype.startDir
-                            val angles = entry.prototype.anglesSignature()
-                            val isPerWorld = isOfTag(actionsRegistry, id, HexTags.Actions.PER_WORLD_PATTERN)
-                        }
-
-                        patterns[id.location().toString()] = obj
-                    }
-
-                    val file = java.io.File("patterns.json")
-                    file.writeText(gson.toJson(patterns))
-
-                    context.source.sendSuccess({-> Component.literal("Dumped actions registry to patterns.json")}, false)
-
-                    1
-                })
-        }
     }
 }
--- common/src/main/kotlin/io/github/jojotastic777/hexdump/HexdumpClient.kt
+++ common/src/main/kotlin/io/github/jojotastic777/hexdump/HexdumpClient.kt
@@ -1,13 +1,52 @@
 package io.github.jojotastic777.hexdump
 
+import at.petrak.hexcasting.api.mod.HexTags
+import at.petrak.hexcasting.api.utils.isOfTag
+import at.petrak.hexcasting.xplat.IXplatAbstractions
+import com.google.gson.Gson
+import dev.architectury.event.events.client.ClientCommandRegistrationEvent
+import dev.architectury.event.events.client.ClientCommandRegistrationEvent.literal
 import io.github.jojotastic777.hexdump.config.HexdumpConfig
 import io.github.jojotastic777.hexdump.config.HexdumpConfig.GlobalConfig
 import me.shedaniel.autoconfig.AutoConfig
 import net.minecraft.client.gui.screens.Screen
+import net.minecraft.network.chat.Component
 
 object HexdumpClient {
     fun init() {
         HexdumpConfig.initClient()
+
+        val actionsRegistry = IXplatAbstractions.INSTANCE.actionRegistry
+        val gson = Gson()
+
+        ClientCommandRegistrationEvent.EVENT.register { dispatcher, _ ->
+            dispatcher.register(literal("hexdump")
+                .executes { context ->
+                    val patterns = HashMap<String, Any>()
+
+                    for (id in actionsRegistry.registryKeySet()) {
+                        val entry = actionsRegistry.get(id)
+                        if (entry == null) { continue }
+
+                        val obj = object {
+                            val id = id.location().toString()
+                            val startDir = entry.prototype.startDir
+                            val angles = entry.prototype.anglesSignature()
+                            val isPerWorld = isOfTag(actionsRegistry, id, HexTags.Actions.PER_WORLD_PATTERN)
+                        }
+
+                        patterns[id.location().toString()] = obj
+                    }
+
+                    val file = java.io.File("patterns.json")
+                    file.writeText(gson.toJson(patterns))
+
+                    context.source.`arch$sendSuccess`({-> Component.literal("Dumped actions registry to patterns.json")}, false)
+
+                    1
+                })
+        }
+
     }
 
     fun getConfigScreen(parent: Screen): Screen {
